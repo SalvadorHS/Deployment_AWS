aed7ffca29c954d51b4ec4a609f9a0ca10440269
Salvador_HS <107784423+SalvadorHS@users.noreply.github.com>
2022-09-14 17:55:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,79 @@
+from flask import Flask, request
 import numpy as np
-from flask import Flask, request, jsonify, render_template
 import pickle
+import pandas as pd
+import flasgger
+from flasgger import Swagger
 
-app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+app=Flask(__name__)
+Swagger(app)
+
+pickle_in = open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def welcome():
+    return "Welcome All"
 
-@app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+@app.route('/predict',methods=["Get"])
+def predict_note_authentication():
+    
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:  
+      - name: variance
+        in: query
+        type: number
+        required: true
+      - name: skewness
+        in: query
+        type: number
+        required: true
+      - name: curtosis
+        in: query
+        type: number
+        required: true
+      - name: entropy
+        in: query
+        type: number
+        required: true
+    responses:
+        200:
+            description: The output values
+        
+    """
+    variance=request.args.get("variance")
+    skewness=request.args.get("skewness")
+    curtosis=request.args.get("curtosis")
+    entropy=request.args.get("entropy")
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return "Hello The answer is"+str(prediction)
 
-    output = round(prediction[0], 2)
+@app.route('/predict_file',methods=["POST"])
+def predict_note_file():
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+      
+    responses:
+        200:
+            description: The output values
+        
+    """
+    df_test=pd.read_csv(request.files.get("file"))
+    print(df_test.head())
+    prediction=classifier.predict(df_test)
+    
+    return str(list(prediction))
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=8000)
+    
+    
